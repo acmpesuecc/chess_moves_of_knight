6438be48e246f2478f7ebb111df7611f2cef8bb5
anirudhRowjee <ani.rowjee@gmail.com>
2020-10-25 22:40:05 +0530

    Closes #5
    Changelog
    1. Minor Documentation fixes
    2. replaced `gets()` with `scanf()`

--- KnightMain.c
+++ KnightMain.c
@@ -8,13 +8,16 @@
 int main()
 {
 	Hash *table=createHash();
+
 	printf("The position in a chess board is given as A1,A2,A3...B1,B2,B3...H6,H7,H8 where the alphabet represents the row number and the number represents the column number.\nThe values returned on entering the position when prompted is the next possible positions that a knight can move on a chess board.\n");
+
 	while(1)
 	{
 		char x[10];
 		printf("\n");
 		printf("Enter position on chess board. Enter 0 to exit:");
-		gets(x);
+		// gets(x);
+		scanf("%s", x);
 		if(*x=='0')
 			break;
 		else if(strlen(x)!=2)
@@ -26,6 +29,8 @@ int main()
 		userinput(x[0],y,table);
 		fflush(stdin);
 	}
+
 	free(table);
+
 	return 0;
 }
